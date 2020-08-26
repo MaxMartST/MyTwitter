8fe9f0f9cc9f2ad191c49bd0f3b7cc8e2b59bb03
Max <addergtx@gmail.com>
2020-08-26 21:18:08 +0300

    implemented a modular structure for a react project

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css'
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Max Mart</h1>
+            <h2>5 записей, из инх понравилось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
