ef36acb89d14d2823f5bc35d9e22035c17e366f1
Max <addergtx@gmail.com>
2020-08-30 00:11:01 +0300

    add event onClick Добавить and add posts, filter like post and all post

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './app-header.css'
 
-const AppHeader = () => {
+const AppHeader = ({liked, allPosts}) => {
     return (
         <div className="app-header d-flex">
             <h1>Max Mart</h1>
-            <h2>5 записей, из инх понравилось 0</h2>
+            <h2>{allPosts} записей, из инх понравилось {liked}</h2>
         </div>
     )
 }
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,14 +2,19 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css'
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLike}) => {
 
     const elements = posts.map((item) => {
         const {id, ...itemProps} = item;
 
         return (
             <li key={id} className="list-group-item">
-                <PostListItem {...itemProps} onDelete={() => onDelete(id)}/>
+                <PostListItem 
+                    {...itemProps} 
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLike={() => onToggleLike(id)}
+                />
             </li>
         )
     });
